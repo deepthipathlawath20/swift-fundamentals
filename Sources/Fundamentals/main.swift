45a35d6aab41acee148cd6286661bacb38561589
Deepthi Pathlawath <deepthipathlawath@gmail.com>
2025-10-01 12:07:42 -0500

    Create main.swift

---
+++ Sources/Fundamentals/main.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+print("== Swift Fundamentals Demo ==")
+print("Add 2+3=\(MathUtils.add(2,3))")
+print("Mul 4*5=\(MathUtils.mul(4,5))")
+print("Mean [1,2,3]=\(MathUtils.mean([1,2,3]))")
+
+print(StringUtils.greet(name: "Deepthi"))
+print(StringUtils.greet(name: nil))
